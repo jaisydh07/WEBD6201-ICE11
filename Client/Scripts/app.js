dd0316a0377fd12860e0c0133520718e54edbcf5
jaisydh07 <jasmailkaur.dhaliwal@dcmail.ca>
2021-04-10 19:20:45 -0400

    added login controller

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -75,69 +75,39 @@ var core;
         formValidation();
     }
     function displayLogin() {
-        let messageArea = $("#messageArea");
-        messageArea.hide();
-        $("#loginButton").on("click", function () {
-            let username = $("#username");
-            let password = $("#password");
-            let success = false;
-            let newUser = new core.User();
-            $.get("./Data/users.json", function (data) {
-                for (const user of data.users) {
-                    if (username.val() == user.Username && password.val() == user.Password) {
-                        newUser.fromJSON(user);
-                        success = true;
-                        break;
-                    }
-                }
-                if (success) {
-                    sessionStorage.setItem("user", newUser.serialize());
-                    messageArea.removeAttr("class").hide();
-                    $("form").trigger("submit");
-                }
-                else {
-                    username.trigger("focus").trigger("select");
-                    messageArea.show().addClass("alert alert-danger").text("Error: Invalid login information");
-                }
-            });
-        });
-        $("#cancelButton").on("click", function () {
-            document.forms[0].reset();
-            location.href = '/home';
-        });
-    }
-    function performLogout() {
-        sessionStorage.clear();
-        location.href = '/login';
-    }
-    function authGuard() {
-        if (!sessionStorage.getItem("user")) {
+        function performLogout() {
+            sessionStorage.clear();
             location.href = '/login';
         }
-    }
-    function display404() {
-    }
-    function Start() {
-        let pageID = $("body")[0].getAttribute("id");
-        switch (pageID) {
-            case 'edit':
-                displayEdit();
-                break;
-            case 'contact':
-                displayContact();
-                break;
-            case 'login':
-                displayLogin();
-                break;
-            case 'logout':
-                performLogout();
-            case 'register':
-                break;
-            case 'contact-list':
-                displayContactList();
-                break;
+        function authGuard() {
+            if (!sessionStorage.getItem("user")) {
+                location.href = '/login';
+            }
         }
+        function display404() {
+        }
+        function Start() {
+            let pageID = $("body")[0].getAttribute("id");
+            switch (pageID) {
+                case 'edit':
+                    displayEdit();
+                    break;
+                case 'contact':
+                    displayContact();
+                    break;
+                case 'login':
+                    displayLogin();
+                    break;
+                case 'logout':
+                    performLogout();
+                case 'register':
+                    break;
+                case 'contact-list':
+                    displayContactList();
+                    break;
+            }
+        }
+        window.addEventListener("load", Start);
     }
-    window.addEventListener("load", Start);
 })(core || (core = {}));
 //# sourceMappingURL=app.js.map
