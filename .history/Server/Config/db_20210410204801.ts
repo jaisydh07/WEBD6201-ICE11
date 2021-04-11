dca294dd77c6e4264ffa34e58955a13c13ebf187
jaisydh07 <jasmailkaur.dhaliwal@dcmail.ca>
2021-04-10 20:52:44 -0400

    switch to MongoDB Atlas

--- .history/Server/Config/db_20210410204801.ts
+++ .history/Server/Config/db_20210410204801.ts
@@ -1,6 +1,6 @@
 export const URI = "mongodb://localhost/webd6201";
 export const LocalURI = "mongodb://localhost/webd6201";
-export const RemoteURI = "mongodb+srv://jaisydh07:Dhaliwal99@webd6201.4ryhk.mongodb.net/webd6201?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://jaisydh07:<password>@webd6201.4ryhk.mongodb.net/webd6201?retryWrites=true&w=majority";
 export const Secret = "someSecret";
 
 export const Host = "MongoDB Atlas";
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.URI = void 0;
+exports.Host = exports.Secret = exports.RemoteURI = exports.LocalURI = exports.URI = void 0;
 exports.URI = "mongodb://localhost/webd6201";
+exports.LocalURI = "mongodb://localhost/webd6201";
+exports.RemoteURI = "mongodb+srv://jaisydh07:Dhaliwal99@webd6201.4ryhk.mongodb.net/webd6201?retryWrites=true&w=majority";
 exports.Secret = "someSecret";
+exports.Host = "MongoDB Atlas";
 //# sourceMappingURL=db.js.map
